a6b223489ccc1bb2c5aea55acc51367a83fef2b3
gaojing <1344073964@qq.com>
2017-09-12 09:28:29 +0800

    讲师列表中的查看 模态框 完成

--- src/js/teacher/list.js
+++ src/js/teacher/list.js
@@ -4,3 +4,25 @@ require('../common/aside.js');
 $.get('/v6/teacher', function(data) {
     $('#teacher-list-table').append(template('teacher-list-tpl', data.result));
 });
+
+
+// $('.modal-dialog').ajaxForm({
+//     delegation: true,
+//     success: function(data) {
+//         if (data.code == 200) {
+//             alert("查看讲师信息了！");
+//         }
+//     }
+// })
+
+//讲师的模态框渲染
+
+$(document).on('click', '[ href="#teacherModal"]', function() {
+    //获取当前记录的id号
+    var tc_id = $(this).attr('data-tc-id');
+    $.get('/v6/teacher/view', { tc_id: tc_id }, function(data) {
+        $('#teacherModal').html(template('teacher-modal-tpl', data.result));
+
+    })
+
+})
